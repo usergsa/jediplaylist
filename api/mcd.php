8714088ae12ccb74c0d428a9a8150b0100f53cb3
usergsa <106740361+usergsa@users.noreply.github.com>
2024-08-17 20:11:20 -0300

    Update mcd.php

--- api/mcd.php
+++ api/mcd.php
@@ -8,6 +8,15 @@ curl_setopt($ch, CURLOPT_URL, "https://payeer.com/api/trade/info");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HEADER, false);
 
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array(
+'pair' => 'BTC_USD,BTC_RUB',
+)));
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+"Content-Type: application/json"
+));
+
 $response = curl_exec($ch);
 curl_close($ch);
 
