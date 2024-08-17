fd61e08471efcd58151b98266f7fb18d252ecf18
usergsa <106740361+usergsa@users.noreply.github.com>
2024-08-17 19:59:23 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,9 +1,40 @@
 <!DOCTYPE html>
 <html>
-  <body>
-    <h1>Estamos aprendendo PHP!</h1>
-    <?php
-      echo "Vamos prosseguir aprendendo PHP";
-    ?>
-  </body>
+<body>
+  
+<?php
+$msec = round(microtime(true) * 1000);
+
+$apiId = '97d91c93-9ee9-422d-89ce-a997bf3f5c24';
+$apiSecret = 'yOHSNVyAycLwWQxQ';
+
+$method = 'account';
+
+$req = json_encode(array(
+'ts' => $msec,
+));
+
+$sign = hash_hmac('sha256', $method.$req, $apiSecret);
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://payeer.com/api/trade/".$method);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, false);
+
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+"Content-Type: application/json",
+"API-ID: ".$apiId,
+"API-SIGN: ".$sign
+));
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+echo '<pre>'; print_r(json_decode($response, true)); echo '</pre>';
+?>
+</body>
 </html>
