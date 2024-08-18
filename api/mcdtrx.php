97e2f0d4bcd8da18773aa1f89379013588774c59
usergsa <106740361+usergsa@users.noreply.github.com>
2024-08-17 22:37:47 -0300

    Update mcdtrx.php

--- api/mcdtrx.php
+++ api/mcdtrx.php
@@ -14,8 +14,8 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 "Content-Type: application/json"
 ));
 
-$response = curl_exec($ch);
+$response = curl_exec($ch)=>pairs;
 curl_close($ch);
 
-echo print_r(json_decode($response>pairs, true));
+echo print_r(json_decode($response, true));
 ?>
