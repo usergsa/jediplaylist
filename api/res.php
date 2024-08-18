e18f2b4fb5bbd9b073658f3dfc760de1f43de3b3
usergsa <106740361+usergsa@users.noreply.github.com>
2024-08-18 09:12:15 -0300

    Update res.php

--- api/res.php
+++ api/res.php
@@ -12,33 +12,33 @@
 <script>
     const canvas = document.getElementById("meuGrafico");
     const ctx = canvas.getContext("2d");
-    
+
 
     // Largura e altura das barras
     const larguraBarra = 50;
     const alturaMaxima = 200;
     dados = [0, 0, 0, 0];
-    trx=0
- 
-function loop() {
-fetch('https://jediplaylist.vercel.app/api/mcdtrx.php') // Substitua pela URL desejada
-  .then(response => response.text())
-  .then(text => {
-    try {
-      const jsonData = JSON.parse(text);
-      console.log('Dados em formato JSON:', jsonData.pairs.TRX_RUB);
-      trx=jsonData.pairs.TRX_RUB.ask
-      // Faça o que desejar com os dados aqui
-    } catch (error) {
-      console.error('Erro ao fazer parsing do JSON:', error);
-    }
-  })
-  .catch(error => {
-    console.error('Erro ao buscar dados:', error);
-  });
+    trx = 0
+
+    function loop() {
+        fetch('https://jediplaylist.vercel.app/api/mcdtrx.php') // Substitua pela URL desejada
+            .then(response => response.text())
+            .then(text => {
+                try {
+                    const jsonData = JSON.parse(text);
+                    trx = jsonData.pairs.TRX_RUB.ask
+                    //console.log('Dados em formato JSON:', jsonData.pairs.TRX_RUB);
+                    // Faça o que desejar com os dados aqui
+                } catch (error) {
+                    console.error('Erro ao fazer parsing do JSON:', error);
+                }
+            })
+            .catch(error => {
+                console.error('Erro ao buscar dados:', error);
+            });
 
 
-        
+
         // Dados do gráfico (valores das barras)
         dados = [dados[1], dados[2], dados[3], trx];
         ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -51,12 +51,9 @@ fetch('https://jediplaylist.vercel.app/api/mcdtrx.php') // Substitua pela URL de
         }
     }
 
-    function getRand(min, max) {
-        return Math.random() * (max - min) + min;
-    }
-
-
-
+    //function getRand(min, max) {
+    //    return Math.random() * (max - min) + min;
+    //}
     //https://jediplaylist.vercel.app/api/mcdtrx.php
 
 
